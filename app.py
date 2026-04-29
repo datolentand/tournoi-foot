4b89849546ac51910bc907f116125ca0c6b89fb4
datolentand <datolentand@gmail.com>
2026-04-29 12:40:24 +0100

    fix SyntaxError port

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def liste_equipes():
     
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.-get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
 
